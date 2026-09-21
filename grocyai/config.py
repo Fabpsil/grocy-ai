2be76d91d8c73a5cc679e064e00b66fd61fbba9e
Fabpsil <fabpsil@live.com>
2026-09-21 14:59:20 -0300

    Delete grocyai/config.py

--- grocyai/config.py
+++
@@ -1,8 +0,0 @@
-import json
-
-with open('/data/options.json') as f:
-    config = json.load(f)
-
-GROCY_URL = config.get("grocy_url")
-GROCY_API_KEY = config.get("grocy_api_key")
-GEMINI_API_KEY = config.get("gemini_api_key")
